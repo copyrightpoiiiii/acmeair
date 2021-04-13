f6ba601ed75897d61acc5bce83d126950722a902
copyrightpoiiiii <barcelonakeyfc@qq.com>
2021-04-13 17:05:47 +0800

    update

--- Jmeter_batch.ps1
+++ Jmeter_batch.ps1
@@ -1,6 +1,6 @@
-param($acmeair_ip,[int]$acmeair_port)
-for($i=0;$i -le 10;$i+=2)
+param($acmeair_ip,[int]$acmeair_port,[int]$size)
+for($i=1;$i -le $size;$i++)
 {
-    write-host $acmeair_ip : ($port=$acmeair_port+$i)
+    write-host $acmeair_ip : ($port=$acmeair_port+3*($i-1))
     docker run -i -t -P -d -e APP_PORT_9080_TCP_ADDR=$acmeair_ip -e APP_PORT_9080_TCP_PORT=$port copyright.azurecr.io/acmeair/workload
 }
