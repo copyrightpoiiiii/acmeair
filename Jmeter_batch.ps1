740b66aa4d4a538d16e2222d11f167a7e8cf5ef0
copyrightpoiiiii <barcelonakeyfc@qq.com>
2021-04-14 09:33:26 +0800

    update

--- Jmeter_batch.ps1
+++ Jmeter_batch.ps1
@@ -4,3 +4,12 @@ for($i=1;$i -le $size;$i++)
     write-host $acmeair_ip : ($port=$acmeair_port+3*($i-1))
     docker run -i -t -P -d -e APP_PORT_9080_TCP_ADDR=$acmeair_ip -e APP_PORT_9080_TCP_PORT=$port copyright.azurecr.io/acmeair/workload
 }
+
+$containers = docker ps -aq
+
+for($i=0;$i -lt $size;$i++)
+{
+    docker logs $containers[i]
+}
+
+docker rm $(docker ps -aq)
